12b14924670f461c6f4d71b24421d2ea5303ab68
Iswar <rick731202@gmail.com>
2022-06-23 21:33:35 +0530

    small bug fix

--- src/components/SingleQuestion.jsx
+++ src/components/SingleQuestion.jsx
@@ -9,7 +9,7 @@ function SingleQuestion(props) {
         <>
             <div className='flex justify-between my-3'>
                 <div className="select-none">
-                    {props.question.question}
+                    {props.question?.question}
                 </div>
                 <div>
                     <Link className='text-violet-500 mx-1' to={{ pathname: `/edit/${props.index}`, query: { id: props.index } }}>Edit</Link>
